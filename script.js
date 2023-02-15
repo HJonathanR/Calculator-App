68f1d8a32a50d4a091181b8ed4dad70cf690ef91
Jonathan <hramos30498@gmail.com>
2023-02-15 13:22:08 -0600

    added sum, subtract, divide, and multiply functions to script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+//functions for calculator
+const add = (number1, number2) => {
+    const total = number1 + number2;
+    return total;
+}
+
+const subtract = (number1, number2) => {
+    const total = number1 - number2;
+    return total;
+}
+
+const multiply = (number1, number2) => {
+    const total = number1 * number2;
+    return total;
+}
+
+const divide = (number1, number2) => {
+    const total = number1 / number2;
+    return total;
+}
